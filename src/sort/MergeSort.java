b89d40af3fbda7b611e34b6328a19f05f298aae3
flyingdream-500 <57061500+flyingdream-500@users.noreply.github.com>
2024-08-17 22:33:19 +0300

    Added selection sort

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,2 +1,16 @@
-package sort;public class MergeSort {
+package sort;
+
+import sort.base.AbstractSort;
+
+public class MergeSort extends AbstractSort {
+
+    @Override
+    public void sort(int[] array) {
+
+    }
+
+    public static void main(String[] args) {
+        AbstractSort mergeSort = new MergeSort();
+        mergeSort.execute();
+    }
 }
--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,2 +1,26 @@
-package sort;public class SelectionSort {
+package sort;
+
+import sort.base.AbstractSort;
+
+import static utils.ArrayUtils.swapTwoItems;
+
+public class SelectionSort extends AbstractSort {
+
+    @Override
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swapTwoItems(array, i, minIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        AbstractSort selectionSort = new SelectionSort();
+        selectionSort.execute();
+    }
 }
